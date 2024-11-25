adb7428015532eb41be64adb982036b3317fa008
arjungravi1@gmail.com <arjungravi1@gmail.com>
2024-11-25 12:17:12 +0530

    Committed

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function showMessage(){
     for (i=0; i<10; i++){
-        document.write('test')
+        // document.write('test');
+        // document.write(Math.ceil(10.3));
+        var d = Date(2002, 3, 9);
+        document.write(d);
+        document.write('<br>')
 
     }
 }
